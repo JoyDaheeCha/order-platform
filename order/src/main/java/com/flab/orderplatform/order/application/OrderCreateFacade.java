08cbae9ee412725253a65e6d2e4ff635b95438e4
JoyDaheeCha <daheecha0612@gmail.com>
2026-07-21 19:17:47 +0900

    feat: 주문 생성시, 상품코드가 DB에 존재하지 않을 경우 예외 throw하라

--- order/src/main/java/com/flab/orderplatform/order/application/OrderCreateFacade.java
+++ order/src/main/java/com/flab/orderplatform/order/application/OrderCreateFacade.java
@@ -2,12 +2,14 @@ package com.flab.orderplatform.order.application;
 
 import com.flab.orderplatform.order.application.annotation.OrderTransactional;
 import com.flab.orderplatform.order.application.command.OrderCreateCommand;
+import com.flab.orderplatform.order.application.exception.ProductNotFoundException;
 import com.flab.orderplatform.order.application.port.out.ProductRepository;
 import com.flab.orderplatform.order.domain.OrderNumberGenerator;
 import com.flab.orderplatform.order.domain.external.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,13 +27,24 @@ public class OrderCreateFacade {
     @OrderTransactional
     public Long createOrder(OrderCreateCommand command) {
         var orderNumber = orderNumberGenerator.generate();
-        var pricesByProductCode = createPriceByProductCodeMap(command.getProductCodes());
-        var createdOrder = orderCommandHandler.handle(orderNumber, pricesByProductCode, command);
+        var productsByCode = createProductByProductCodeMap(command.getProductCodes());
+        var createdOrder = orderCommandHandler.handle(orderNumber, productsByCode, command);
         return createdOrder.getId();
     }
 
-    public Map<String, Product> createPriceByProductCodeMap(List<String> productCodes) {
+    public Map<String, Product> createProductByProductCodeMap(List<String> productCodes) {
         var products = productRepository.findAllByProductCodeIn(productCodes);
-        return products.stream().collect(Collectors.toMap(Product::getProductCode, p -> p));
+
+        var productsByProductCodeMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductCode, p -> p));
+
+        // 모든 상품코드가 유효하면 정상
+        if (productCodes.size() == productsByProductCodeMap.size()) {
+            return productsByProductCodeMap;
+        }
+
+        var notFound = new LinkedHashSet<>(productCodes);
+        notFound.removeAll(productsByProductCodeMap.keySet());
+        throw new ProductNotFoundException(notFound);
     }
 }
