13c8e1f37856d43fa4ae98f874f21b4b54487326
JoyDaheeCha <daheecha0612@gmail.com>
2026-07-24 00:00:21 +0900

    fix: 주문번호 생성기 bean으로 등록

--- order/src/main/java/com/flab/orderplatform/order/infrastructure/config/OrderDomainConfig.java
+++ order/src/main/java/com/flab/orderplatform/order/infrastructure/config/OrderDomainConfig.java
@@ -1,5 +1,6 @@
 package com.flab.orderplatform.order.infrastructure.config;
 
+import com.flab.orderplatform.order.domain.OrderNumberGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -10,6 +11,11 @@ import java.util.Optional;
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "orderAuditorAware")
 public class OrderDomainConfig {
+
+    @Bean
+    OrderNumberGenerator orderNumberGenerator() {
+        return new OrderNumberGenerator();
+    }
 
     // TODO: 인증/인가 도입되면 SecurityContext 에서 현재 사용자를 가져오도록 교체
     @Bean
