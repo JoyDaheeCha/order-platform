468aa8f292b102e381f2175f6bb60b12721137ac
JoyDaheeCha <daheecha0612@gmail.com>
2026-07-30 10:33:48 +0900

    feat: 결제 전용 트랜잭션 애노테이션 PaymentTransactional 추가하라
    payment 내에서만 트랜잭션 동작할 수 있도록 로직 추가

--- payment/src/main/java/com/flab/orderplatform/payment/application/PaymentCommandHandler.java
+++ payment/src/main/java/com/flab/orderplatform/payment/application/PaymentCommandHandler.java
@@ -1,5 +1,6 @@
 package com.flab.orderplatform.payment.application;
 
+import com.flab.orderplatform.payment.application.annotation.PaymentTransactional;
 import com.flab.orderplatform.payment.application.command.PaymentCompleteCommand;
 import com.flab.orderplatform.payment.application.command.PaymentCreateCommand;
 import com.flab.orderplatform.payment.application.port.out.PaymentRepository;
@@ -7,7 +8,6 @@ import com.flab.orderplatform.payment.common.exception.PaymentNotFoundException;
 import com.flab.orderplatform.payment.domain.Payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,12 +19,11 @@ import static com.flab.orderplatform.payment.domain.status.PaymentStatus.REQUEST
 public class PaymentCommandHandler {
     private final PaymentRepository paymentRepository;
 
-    // TODO 별도 Tansaction으로 만들기
     /**
      * 신규 결제 정보를 생성한다.
      * - 이미 결제가 요청되었거나 실패한 경우도 결제 시도 대상에 포함한다.
      */
-    @Transactional
+    @PaymentTransactional
     public Payment handle(PaymentCreateCommand command) {
         var orderNumber = command.orderNumber();
         var payment = paymentRepository.findByOrderNumberAndStatusIn(orderNumber, List.of(REQUESTED, FAILED))
@@ -32,7 +31,7 @@ public class PaymentCommandHandler {
         return paymentRepository.save(payment);
     }
 
-    @Transactional
+    @PaymentTransactional
     public Payment handle(PaymentCompleteCommand command) {
         var orderNumber = command.orderNumber();
         var requestedPayment = paymentRepository.findByOrderNumberAndStatus(orderNumber, REQUESTED)
