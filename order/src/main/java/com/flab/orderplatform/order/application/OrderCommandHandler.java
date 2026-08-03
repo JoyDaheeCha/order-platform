347367f280ec6bc3777c51eb27217da831dff9b7
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-03 17:02:08 +0900

    adjust: command를 POJO처럼 단순화하라

--- order/src/main/java/com/flab/orderplatform/order/application/OrderCommandHandler.java
+++ order/src/main/java/com/flab/orderplatform/order/application/OrderCommandHandler.java
@@ -4,6 +4,7 @@ import com.flab.orderplatform.order.application.annotation.OrderTransactional;
 import com.flab.orderplatform.order.application.command.OrderCreateCommand;
 import com.flab.orderplatform.order.application.port.out.OrderRepository;
 import com.flab.orderplatform.order.domain.Order;
+import com.flab.orderplatform.order.domain.OrderItem;
 import com.flab.orderplatform.order.domain.external.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -19,7 +20,18 @@ public class OrderCommandHandler {
 
     @OrderTransactional
     public Order handle(String orderNumber, Map<String, Product> productMap, OrderCreateCommand command) {
-        var order = orderRepository.save(command.createOrder(orderNumber, productMap));
+        var orderItems = command.getOrderItems().stream().map(item -> {
+                    var product = productMap.get(item.productCode());
+                    return OrderItem.builder()
+                            .quantity(item.quantity())
+                            .name(item.name())
+                            .productId(product.getId())
+                            .price(product.getPrice())
+                            .quantity(item.quantity())
+                            .build();
+                })
+                .toList();
+        var order = orderRepository.save(command.createOrder(orderNumber, orderItems));
         eventPublisher.publishEvent(order.pullDomainEvent());
         return order;
     }
