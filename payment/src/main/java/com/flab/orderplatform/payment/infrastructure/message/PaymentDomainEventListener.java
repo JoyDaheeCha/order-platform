284ae314b20ab526e239c53d3419577a62b1342b
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-12 00:22:04 +0900

    feat: payment 모듈에서 inbox 기능 추가

--- payment/src/main/java/com/flab/orderplatform/payment/infrastructure/message/PaymentDomainEventListener.java
+++ payment/src/main/java/com/flab/orderplatform/payment/infrastructure/message/PaymentDomainEventListener.java
@@ -1,21 +1,22 @@
 package com.flab.orderplatform.payment.infrastructure.message;
 
-import com.flab.orderplatform.payment.application.annotation.Inbox;
+import com.flab.orderplatform.shared.inbox.Inbox;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import static com.flab.orderplatform.shared.event.EventConstants.AGGREGATE_PAYMENT;
 import static com.flab.orderplatform.shared.event.EventConstants.ORDER_CREATED_TOPIC;
 
 @Slf4j
 @Component
 public class PaymentDomainEventListener {
 
-    @Inbox
+    @Inbox(AGGREGATE_PAYMENT)
     @KafkaListener(topics = ORDER_CREATED_TOPIC, groupId = "payment")
     public void handle(ConsumerRecord<String, String> payload) {
         log.debug("[OrderCreatedEvent] payload: {}", payload.value());
-        // InboxAspect 가 인박스 저장 담당. 로직처리는 InboxEventScheduler 에 있다.
+        // InboxAspect 가 인박스 저장 담당. 로직처리는 PaymentInboxEventScheduler 에 있다.
     }
 }
---
+++ payment/src/main/java/com/flab/orderplatform/payment/infrastructure/message/inbox/PaymentInboxEventProcessor.java
@@ -0,0 +1,6 @@
+package com.flab.orderplatform.payment.infrastructure.message.inbox;
+
+import com.flab.orderplatform.shared.inbox.InboxEventProcessor;
+
+public interface PaymentInboxEventProcessor extends InboxEventProcessor {
+}
