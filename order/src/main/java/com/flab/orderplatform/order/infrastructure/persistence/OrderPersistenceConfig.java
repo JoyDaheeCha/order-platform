04b4802bc6199611b90134a6555280febbc04a79
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-11 23:31:26 +0900

    config: DB 형상관리를 flyway로 하라

--- order/src/main/java/com/flab/orderplatform/order/infrastructure/persistence/OrderPersistenceConfig.java
+++ order/src/main/java/com/flab/orderplatform/order/infrastructure/persistence/OrderPersistenceConfig.java
@@ -1,6 +1,7 @@
 package com.flab.orderplatform.order.infrastructure.persistence;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.flywaydb.core.Flyway;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -42,6 +44,20 @@ public class OrderPersistenceConfig {
         return orderDataSourceProperties().initializeDataSourceBuilder().build();
     }
 
+    /**
+     * Order 컨텍스트 전용 Flyway 마이그레이션 빈
+     */
+    @Bean(initMethod = "migrate")
+    public Flyway orderFlyway(@Qualifier("orderDataSource") DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .defaultSchema("order_schema")
+                .locations("classpath:db/migration/order")
+                .baselineOnMigrate(true)
+                .load();
+    }
+
+    @DependsOn("orderFlyway")
     @Bean
     LocalContainerEntityManagerFactoryBean orderEntityManagerFactory(
             @Qualifier("orderDataSource") DataSource dataSource,
