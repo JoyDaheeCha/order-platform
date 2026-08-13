50e6e45b5f072b3df23d3f600d9d84f5ee33354b
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-13 19:05:28 +0900

    refactor: 기동 시점에 오타 감지하라

--- inventory/src/main/java/com/flab/orderplatform/inventory/application/InventoryDecreaseLockResolver.java
+++ inventory/src/main/java/com/flab/orderplatform/inventory/application/InventoryDecreaseLockResolver.java
@@ -1,6 +1,7 @@
 package com.flab.orderplatform.inventory.application;
 
 import com.flab.orderplatform.inventory.application.port.out.InventoryDecreaseCommandHandler;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,19 @@ public class InventoryDecreaseLockResolver {
     @Value("${inventory.lock-strategy:distributed}")
     private String strategy;
 
+    @PostConstruct
+    void validate() {
+        if (!handler.containsKey(beanName())) {
+            throw new IllegalStateException(
+                    "지원하지 않는 락 전략입니다: %s (가능: %s)".formatted(strategy, handler.keySet()));
+        }
+    }
+
     public InventoryDecreaseCommandHandler resolve() {
-        return handler.get(strategy + "LockInventoryDecreaseCommandHandler");
+        return handler.get(beanName());
+    }
+
+    private String beanName() {
+        return strategy + "LockInventoryDecreaseCommandHandler";
     }
 }
