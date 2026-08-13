df90aff5700163654f23160f02195ea5d8725f18
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-13 19:37:07 +0900

    fix: 비관락 타임아웃 3초로 제한

--- inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPessimisticLockStrategy.java
+++ inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPessimisticLockStrategy.java
@@ -2,11 +2,9 @@ package com.flab.orderplatform.inventory.infrastructure.persistence;
 
 import com.flab.orderplatform.inventory.domain.Inventory;
 import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
@@ -16,9 +14,6 @@ public interface InventoryPessimisticLockStrategy extends JpaRepository<Inventor
     /**
      * 재고 감소시 비관락 적용 (타임아웃 3초)
      */
-    @QueryHints({
-            @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")
-    })
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select i from Inventory i where i.productCode in :productCodes order by i.productCode")
     Set<Inventory> findByProductCodeInOrderbyProductCode(@Param("productCodes") Set<String> productCodes);
