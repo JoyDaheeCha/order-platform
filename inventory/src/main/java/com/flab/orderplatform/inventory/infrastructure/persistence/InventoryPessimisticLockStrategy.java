3501759ceab76fbc7cb6cb124c16594ed75b9885
JoyDaheeCha <daheecha0612@gmail.com>
2026-08-13 17:35:54 +0900

    fix: 쿼리 실행시 @param으로 파라메터명 명시

--- inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPessimisticLockStrategy.java
+++ inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPessimisticLockStrategy.java
@@ -5,11 +5,12 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface InventoryPessimisticLockStrategy extends JpaRepository<Inventory, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select i from Inventory i where i.productCode in :productCodes")
-    List<Inventory> findByProductCodeIn(List<String> productCodes);
+    List<Inventory> findByProductCodeIn(@Param("productCodes") List<String> productCodes);
 }
