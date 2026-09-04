e0f5f218508ef21275d0fe26d01e155436152af3
JoyDaheeCha <daheecha0612@gmail.com>
2026-09-04 18:51:15 +0900

    modify: 결제에서 소비할 메시지를 미리 추가해두어라

--- payment/src/main/java/com/flab/orderplatform/payment/infrastructure/message/PaymentDomainEventListener.java
+++ payment/src/main/java/com/flab/orderplatform/payment/infrastructure/message/PaymentDomainEventListener.java
@@ -7,13 +7,14 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import static com.flab.orderplatform.shared.event.EventConstants.AGGREGATE_PAYMENT;
+import static com.flab.orderplatform.shared.event.EventConstants.ORDER_PAYMENT_PREPARED_TOPIC;
 
 @Slf4j
 @Component
 public class PaymentDomainEventListener {
 
     @Inbox(AGGREGATE_PAYMENT)
-    @KafkaListener(topics = {}, groupId = "payment")
+    @KafkaListener(topics = {ORDER_PAYMENT_PREPARED_TOPIC}, groupId = "payment")
     public void handle(ConsumerRecord<String, String> payload) {
         log.debug("[OrderCreatedEvent] payload: {}", payload.value());
         // InboxAspect 가 인박스 저장 담당. 로직처리는 PaymentInboxEventScheduler 에 있다.
--- shared/src/main/java/com/flab/orderplatform/shared/event/EventConstants.java
+++ shared/src/main/java/com/flab/orderplatform/shared/event/EventConstants.java
@@ -13,6 +13,8 @@ public final class EventConstants {
     public static final String ORDER_CREATED_TOPIC = "MSG-ORDER-CREATED";
     public static final String ORDER_PAID = "OrderPaid";
     public static final String ORDER_PAID_TOPIC = "MSG-ORDER-PAID";
+    public static final String ORDER_PAYMENT_PREPARED = "OrderPaymentPrepared"; // TODO 이벤트 추가
+    public static final String ORDER_PAYMENT_PREPARED_TOPIC = "MSG-ORDER-PAYMENT-PREPARED";
     public static final String ORDER_CONFIRMED = "OrderConfirmed";
     public static final String ORDER_CONFIRMED_TOPIC = "MSG-ORDER-CONFIRMED";
     public static final String ORDER_CANCELLATION_REQUESTED = "OrderCancellationRequested";
