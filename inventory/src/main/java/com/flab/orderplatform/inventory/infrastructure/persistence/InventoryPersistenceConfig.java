68a5d4e425339eaee3827c4e09220c8c16d63e00
JoyDaheeCha <daheecha0612@gmail.com>
2026-09-04 16:15:38 +0900

    chore: merge 충돌 제거

--- inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPersistenceConfig.java
+++ inventory/src/main/java/com/flab/orderplatform/inventory/infrastructure/persistence/InventoryPersistenceConfig.java
@@ -78,19 +78,6 @@ public class InventoryPersistenceConfig {
         return emf;
     }
 
-    /**
-     * Inventory 컨텍스트 전용 Flyway 마이그레이션 빈
-     */
-    @Bean(initMethod = "migrate")
-    public Flyway inventoryFlyway(@Qualifier("inventoryDataSource") DataSource dataSource) {
-        return Flyway.configure()
-                .dataSource(dataSource)
-                .defaultSchema("inventory_schema")
-                .locations("classpath:db/migration/inventory")
-                .baselineOnMigrate(true)
-                .load();
-    }
-
     @DependsOn("inventoryFlyway")
     @Bean
     PlatformTransactionManager inventoryTransactionManager(
