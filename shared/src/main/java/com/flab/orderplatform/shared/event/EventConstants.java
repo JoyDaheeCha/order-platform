d568b57d79b12e974d688811f4411856858a1810
JoyDaheeCha <daheecha0612@gmail.com>
2026-09-04 19:12:30 +0900

    chore: 오타 수정

--- shared/src/main/java/com/flab/orderplatform/shared/event/EventConstants.java
+++ shared/src/main/java/com/flab/orderplatform/shared/event/EventConstants.java
@@ -32,7 +32,7 @@ public final class EventConstants {
     public static final String INVENTORY_RESERVED = "InventoryReserved";
     public static final String INVENTORY_RESERVED_TOPIC = "MSG-INVENTORY-RESERVED";
     public static final String INVENTORY_RESERVATION_FAILED = "InventoryReservationFailed";
-    public static final String INVENTORY_RESERVATION_FAILED_TOPIC = "MSG-INVENTORY-RESERVATION_FAILED";
+    public static final String INVENTORY_RESERVATION_FAILED_TOPIC = "MSG-INVENTORY-RESERVATION-FAILED";
     public static final String STOCK_DEDUCTED = "StockDeducted";
     public static final String STOCK_DEDUCTED_TOPIC = "MSG-STOCK-DEDUCTED";
     public static final String STOCK_SHORTAGE = "StockShortage";
