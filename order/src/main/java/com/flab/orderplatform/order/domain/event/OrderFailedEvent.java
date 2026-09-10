3ee261e6f3ae742cdb5698be046c779767b26b24
JoyDaheeCha <daheecha0612@gmail.com>
2026-09-10 21:40:39 +0900

    modify: 주문 실패 메시지 내역에 주문 상품 정보 추가

--- order/src/main/java/com/flab/orderplatform/order/domain/event/OrderFailedEvent.java
+++ order/src/main/java/com/flab/orderplatform/order/domain/event/OrderFailedEvent.java
@@ -1,9 +1,11 @@
 package com.flab.orderplatform.order.domain.event;
 
+import com.flab.orderplatform.order.domain.OrderItem;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.flab.orderplatform.shared.event.EventConstants.*;
 
@@ -14,13 +16,21 @@ import static com.flab.orderplatform.shared.event.EventConstants.*;
 public class OrderFailedEvent extends OrderOutboxEvent {
 
     private final String orderNumber;
+    private final List<OrderItemDto> orderItems;
 
     @Builder
     protected OrderFailedEvent(String aggregateId,
                                LocalDateTime occurredOn,
-                               String orderNumber) {
+                               String orderNumber,
+                               List<OrderItem> orderItems) {
         super(aggregateId, occurredOn);
         this.orderNumber = orderNumber;
+        this.orderItems = orderItems.stream()
+                .map(o -> OrderItemDto.builder()
+                        .productCode(o.getProductCode())
+                        .quantity(o.getQuantity())
+                        .build())
+                .toList();
     }
 
 
